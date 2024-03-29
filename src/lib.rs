82f6a0e94fbec17269635f6af4e30cb93fdff144
jysndabuio <jysndabu@outlook.com>
2024-03-29 21:03:35 +0100

    Still 3 failed test cases

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,17 @@ pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     // a + b + c = N, where a = m²-n²; b = 2mn; c = m² + n²;
     // The conditions : m > n ; Max value of m is sqrt(N);
 
-    let max_value_m = (sum as f32).sqrt() as u32;
+    let max_value_m = (sum as f64).sqrt() as u32;
 
     for m in 2..=max_value_m {
         for n in 1..m {
-            let mut a:u32 =   m*m - n*n;
-            let mut b:u32 = 2 * m * n;
-            let mut c:u32 = m*m + n*n;
+            let a:u32 = m*m - n*n;
+            let b:u32 = 2 * m * n;
+            let c:u32 = m*m + n*n;
             let total:u32 = a + b + c;
             if total == sum {
-                let triplet = [a,b,c];
+                let mut triplet = [a,b,c];
+                triplet.sort();
                 triples.insert(triplet);
             }
         }
